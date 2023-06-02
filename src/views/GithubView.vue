300704ebb2a7908f1bd27c24951f8a98d07708ce
leocastroz <castroz.dev@gmail.com>
2023-06-02 15:55:19 -0300

    Feat: improving component style

--- src/views/GithubView.vue
+++ src/views/GithubView.vue
@@ -1,7 +1,62 @@
 <template>
   <div class="about">
-    <h1>Github ❤️😁</h1>
+    <img class="profile" width="150" src="https://avatars.githubusercontent.com/u/72839343?v=4" alt="">
+    <div class="apresent">
+      <img width="50" src="https://mwcorvettes.com/wp-content/uploads/2019/03/savethewave-300x252.gif" alt="">
+      <h1>Welcome!</h1>
+    </div>
+    <p>I´m Leonardo Castro</p>
+    <p>I´m a <span>Frontend Developer</span></p>
+    <br>
   </div>
 </template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+
+.about {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  font-family: 'Poppins', sans-serif;
+}
+
+.about .profile {
+  border-radius: 100%;
+  margin: 40px 0;
+}
+
+h1 {
+  color: #1c602a;
+}
+
+p {
+  font-weight: 500;
+  margin: 30px 0;
+  font-size: 25px;
+  color: #1c602a;
+}
+
+span {
+  font-weight: 700  ;
+  text-shadow: #00ff33 1px 0 10px;
+  color: #323232;
+}
+
+.apresent {
+  display: flex;
+  flex-direction: row;
+}
+
+.apresent h1 {
+  margin-left: 10px;
+  font-size: 40px;
+  font-weight: 700;
+}
+
+</style>
 
 
--- src/views/HomeFirefoxView.vue
+++ src/views/HomeFirefoxView.vue
@@ -1,4 +1,6 @@
-
+<script>
+import { RouterLink } from 'vue-router'
+</script>
 <template>
   <main>
     <div class="firefox-header">
@@ -11,6 +13,32 @@
         <img width="18" src="https://logopng.com.br/logos/google-37.png" alt="Google">
       </button>
     </div>
+    <div class="sub-items">
+      <RouterLink to="/">
+        <div>
+          <img width="45" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Firefox_logo%2C_2017.svg/800px-Firefox_logo%2C_2017.svg.png" alt="firefox">
+          <h6>Firefox</h6>
+        </div>
+      </RouterLink> 
+      <RouterLink to="/github">
+        <div>
+          <img width="45" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1024px-Octicons-mark-github.svg.png" alt="github">
+          <h6>GitHub</h6>
+        </div>
+      </RouterLink> 
+      <RouterLink to="/instagram">
+        <div>
+          <img width="45" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1024px-Instagram_icon.png" alt="instagram">
+          <h6>Instagram</h6>
+        </div>
+      </RouterLink> 
+      <RouterLink to="/linkedin">
+        <div>
+          <img width="45" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/768px-LinkedIn_logo_initials.png" alt="linkedin">
+          <h6>Linkedin</h6>
+        </div>
+      </RouterLink>
+    </div>
   </main>
 </template>
 
@@ -22,7 +50,7 @@ main {
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 300px;
+  height: 600px;
   color: #fff;
 }
 
@@ -40,7 +68,6 @@ main p {
   align-items: center;
   padding: 40px 0;
 }
-
 
 .submit-lente {
 	position: absolute;
@@ -73,6 +100,50 @@ main p {
 	display: flex;
 	align-items: center;
 
+}
+
+.sub-items {
+  min-width: 700px;
+  display: flex;
+  justify-content: space-between;
+  margin: 40px 0;
+}
+
+.sub-items a {
+  text-decoration: none;
+}
+
+.sub-items div {
+  min-width: 80px;
+  min-height: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+  background-color: #c8c8c8;
+  padding: 20px;
+  border-radius: 5px;
+  color: #7e7e7e;
+  box-shadow: 0 0 2px 0 #000;
+  transition:all 0.3s ease;
+  cursor: pointer;
+}
+
+.sub-items div:hover {
+  box-shadow: 0 0 2px 0 #000;
+  background-color: #fcb14d;
+  color: #fff;
+  -webkit-transform: rotateZ(-30deg);
+  -ms-transform: rotateZ(-30deg);
+  transform: rotateZ(-30deg);
+  width: 80px;
+  height: 80px;
+  font-size: 12px;
+}
+
+.sub-items div h6 {
+  margin-top: 10px;
 }
 
 </style>
